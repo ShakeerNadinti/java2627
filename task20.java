06d3d27b9236a4f4eb5fe665af9e73845411aa66
shakeer_nadinti <darbarbasha034@gmail.com>
2026-08-12 11:32:29 +0530

    Delete task20.java

--- task20.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class java23{
-    public static void main(String[]args){
-        List<Integer>marks =Arrays.asList(45,78,62,90,55,81,38);
-        marks.stream ()
-                .filter(mark->mark>=50).map(mark->mark+5).forEach(mark->System.out.println(mark));
-
-    }
-}
