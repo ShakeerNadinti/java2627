ac6379f014b2658cc26267f8b048c7e1b0270031
shakeer_nadinti <darbarbasha034@gmail.com>
2026-08-12 11:26:11 +0530

    task1.java
    Corrected the salary update logic to double the salaries instead of increasing by 10%.

--- task1.java
+++ task1.java
@@ -3,20 +3,14 @@ import java.util.stream.*;
 public class PSTJTASK1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
-
         List<Integer> salaries = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             salaries.add(sc.nextInt());
         }
-
-        // Functional transformation: increase each salary by 10%
         List<Integer> updatedSalaries = salaries.stream()
                 .map(salary -> (int)(salary * 2))   // increase by 10%
                 .collect(Collectors.toList());
-
-        // Print updated salaries
         updatedSalaries.forEach(sal -> System.out.print(sal + " "));
     }
 }
